453be9820f71d2344f0e0ef4d49c9db3f0a2c3c0
TheStepKla <krtza1@gmail.com>
2022-11-12 17:02:19 +0700

    update 12/11/65 4

--- api/activitys.php
+++ api/activitys.php
@@ -2,19 +2,39 @@
 include('config.php');
 //เช๋คว่ามีค่าเข้ามาหรือไม่
 if(isset($_GET["amount"])){
-
     $amount = $_GET["amount"];
 
-    $conn = new mysqli($database_host, $database_user, $database_password, 'portfoliov2');
-    $result = $conn->query("SELECT * FROM activitys LIMIT ".$amount);
-    $data = $result->fetch_all(MYSQLI_ASSOC);
-    $conn->close();
+    if($amount != 0){
+        $conn = new mysqli($database_host, $database_user, $database_password, 'portfoliov2');
 
-    if(count($data) != 0){
-        echo json_encode($data, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+        $get_count = $conn->query("SELECT * FROM activitys");
+        $count_all = $get_count->num_rows;
+
+        $result = $conn->query("SELECT * FROM activitys LIMIT ".$amount);
+        $data = $result->fetch_all(MYSQLI_ASSOC);
+
+        $conn->close();
+
+        if(count($data) != 0){
+            $json = array("count_all" => $count_all, "data" => $data);
+            echo json_encode($json, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+        }else{
+            $code = array(["status" => "404", "reason" => "ไม่มีข้อมูล"]);
+            echo json_encode($code, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+        }
     }else{
-        $code = array(["status" => "404", "reason" => "ไม่มีข้อมูล"]);
-        echo json_encode($code, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+        $conn = new mysqli($database_host, $database_user, $database_password, 'portfoliov2');
+        $result = $conn->query("SELECT * FROM activitys");
+        $data = $result->fetch_all(MYSQLI_ASSOC);
+        $conn->close();
+
+        if(count($data) != 0){
+            $json = array("count_all" => count($data), "data" => $data);
+            echo json_encode($json, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+        }else{
+            $code = array(["status" => "404", "reason" => "ไม่มีข้อมูล"]);
+            echo json_encode($code, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+        }
     }
 }else{
     $code = array("status" => "404", "reason" => "ไม่ได้กำหนดจำนวนข้อมูลที่ต้องการดึง");
--- api/works.php
+++ api/works.php
@@ -4,17 +4,41 @@ include('config.php');
 if(isset($_GET["amount"])){
     $amount = $_GET["amount"];
 
-    $conn = new mysqli($database_host, $database_user, $database_password, 'portfoliov2');
-    $result = $conn->query("SELECT * FROM works LIMIT ".$amount);
-    $data = $result->fetch_all(MYSQLI_ASSOC);
+    if($amount != 0){
 
-    $conn->close();
+        //ดึงตามความต้องการ
+        $conn = new mysqli($database_host, $database_user, $database_password, 'portfoliov2');
 
-    if(count($data) != 0){
-        echo json_encode($data, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+        $get_count = $conn->query("SELECT * FROM works ");
+        $count_all = $get_count->num_rows;
+        
+        $result = $conn->query("SELECT * FROM works LIMIT ".$amount);
+        $data = $result->fetch_all(MYSQLI_ASSOC);
+
+        $conn->close();
+
+        if(count($data) != 0){
+            $json = array("count_all" => $count_all, "data" => $data);
+            echo json_encode($json, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+        }else{
+            $code = array(["status" => "404", "reason" => "ไม่มีข้อมูล"]);
+            echo json_encode($code, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+        }
     }else{
-        $code = array(["status" => "404", "reason" => "ไม่มีข้อมูล"]);
-        echo json_encode($code, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+        //ดึงข้อมูลทั้งหมด
+        $conn = new mysqli($database_host, $database_user, $database_password, 'portfoliov2');
+        $result = $conn->query("SELECT * FROM works");
+        $data = $result->fetch_all(MYSQLI_ASSOC);
+
+        $conn->close();
+
+        if(count($data) != 0){
+            $json = array("count_all" => count($data), "data" => $data);
+            echo json_encode($json, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+        }else{
+            $code = array(["status" => "404", "reason" => "ไม่มีข้อมูล"]);
+            echo json_encode($code, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+        }
     }
 }else{
     $code = array("status" => "404", "reason" => "ไม่ได้กำหนดจำนวนข้อมูลที่ต้องการดึง");
