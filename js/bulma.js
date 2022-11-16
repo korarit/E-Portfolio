7c7c04b97077ee0f276c3a0d598652b0f20d91ab
TheStepKla <krtza1@gmail.com>
2022-11-16 19:46:03 +0700

    update 16/11/65

--- js/bulma.js
+++ js/bulma.js
@@ -4,3 +4,16 @@ const narbarmenu = document.querySelector('.navbar-menu');
 burgericon.addEventListener('click', () => {
     narbarmenu.classList.toggle('is-active');
 })
+
+
+function nav_sticky(){
+    var navbar = document.getElementById("navbar");
+  
+    if (window.pageYOffset > 0) {
+      navbar.classList.add("is-top")
+    } else {
+      navbar.classList.remove("is-top");
+    }
+  
+}
+window.addEventListener("scroll", nav_sticky);
